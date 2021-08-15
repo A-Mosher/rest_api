de14053e1920b3bac75a64f80feaa3210955b470
A-Mosher <adam.l.mosher@gmail.com>
2021-08-15 19:18:58 -0400

    added a post request to add new users

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,23 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 24
-    }
-]
+const users = [];
 
 router.get('/', (req, res) => {
     console.log(users);
 
     res.send('Hello');
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
 export default router;
